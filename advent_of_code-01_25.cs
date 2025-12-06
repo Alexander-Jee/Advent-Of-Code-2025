027beba1c5dcbe54e6a772f263ec524e34997ef5
Alexander <alexander.jee.t@gmail.com>
2025-12-05 21:53:52 -0600

    refactor, fix issue for edge case where you rotate left when starting at 0, and fix bug where the full dial rotation used the sum counting remainderSteps twice.

--- advent_of_code-01_25.cs
+++ advent_of_code-01_25.cs
@@ -3,41 +3,52 @@
 // Input stayed the same between day 1 and 2
 var inputLines = await File.ReadAllLinesAsync("day1_input.txt");
 // List<string> inputLines = ["L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82"];
-var startNumber = 50;
+// List<string> inputLines = ["L150", "L250"];
+
+var currentPosition = 50;
 var result = 0;
-var timesDialPastZero = 0;
+var fullDialRotations = 0;
 bool rotatingLeft = false;
+
 foreach (var line in inputLines)
 {
     var direction = line[0];
     rotatingLeft = direction == 'L';
-
     var steps = int.Parse(line[1..]);
-    timesDialPastZero = steps / 100;
-    steps %= 100;
+
+    var remainderSteps = steps % 100;
+    fullDialRotations = (steps - remainderSteps) / 100;
 
     if (rotatingLeft)
     {
-        startNumber -= steps;
-        if (startNumber < 0)
+        currentPosition -= remainderSteps;
+        if (currentPosition < 0)
         {
-            startNumber += 100;
-            ++timesDialPastZero;
+            if (currentPosition + remainderSteps != 0)
+            {
+                fullDialRotations++;
+            }
+            currentPosition += 100; // Adjust dial to be on new looped number
         }
     }
 
     if (!rotatingLeft)
     {
-        var originalStartNumber = startNumber;
-        var sum = startNumber + steps;
-        startNumber = sum % 100;
-        if (sum >= 100 || originalStartNumber > startNumber)
+        var sum = currentPosition + remainderSteps;
+        currentPosition = sum % 100;
+
+        if (sum > 100)
         {
-            ++timesDialPastZero;
+            fullDialRotations++;
         }
     }
-    result += timesDialPastZero;
 
-    timesDialPastZero = 0;
+    if (currentPosition == 0)
+    {
+        result++;
+    }
+    result += fullDialRotations;
+    fullDialRotations = 0;
+    Console.WriteLine("Result: {0} Current Position: {1} Remainder Steps: {2} Total Steps: {3}", result, currentPosition, remainderSteps, steps);
 }
 Console.WriteLine(result);
