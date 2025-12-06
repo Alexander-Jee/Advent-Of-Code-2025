46fcd6084a99a45c71a7d4ecd56a80bb1a894a3d
Alexander <alexander.jee.t@gmail.com>
2025-12-05 23:59:26 -0600

    fix comments of initial requirements

--- advent_of_code-02_25.cs
+++ advent_of_code-02_25.cs
@@ -1,8 +1,9 @@
 // Number ID's are given an inclusive range
 // Find the sum of all invalid ID's in range
-// Invalid ID = a number IN RANGE that has ANY number inside of number IN RANGE that repeats TWICE
-// e.g 11-22 = 2 Invalid = 11, 22
-// 99-115 = 1 Invalid = 99
+// Valid ID = a number IN RANGE that has a TWICE repeating number of number IN RANGE
+// e.g 11-22 = 2 Valid = 11, 22
+// e.g. 95 -115 = 1 Valid = 99
+// valid repeating number can not start with 0
 
 // assumptions: no negative ID numbers
 
@@ -14,7 +15,7 @@ List<string> input =
 ];
 // List<string> input = [.. (await File.ReadAllTextAsync("day2_input.txt")).Split(",")];
 
-var result = 0; // Sum of All Invalid Numbers in each range
+var result = 0; // Sum of All Valid Numbers in each range
 
 foreach (var rangeInput in input)
 {
@@ -23,11 +24,19 @@ foreach (var rangeInput in input)
     int endNumber = int.Parse(numbers[1]);
     var tensPlaces = ValidTensPlace(startNumber, endNumber);
     if (tensPlaces.Count == 0) break;
+    int idxValidTensPlace = 0;
+    var currentTensPlace = tensPlaces[idxValidTensPlace];
 
-    for (int i = startNumber; i <= endNumber; i++)
+    for (int i = startNumber; i <= endNumber && idxValidTensPlace < tensPlaces.Count; i++)
     {
         
     }
+}
+
+static bool IsIdValid(int number)
+{
+
+    return false;
 }
 
 static List<int> ValidTensPlace(int startNumber, int endNumber)
