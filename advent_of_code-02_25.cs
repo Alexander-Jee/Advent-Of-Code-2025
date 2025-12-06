9b54d6e10373292c35ccf83b9fea89a74daa907d
Alexander <alexander.jee.t@gmail.com>
2025-12-05 23:05:37 -0600

    create logic to get valid tens places for day 2

--- advent_of_code-02_25.cs
+++ advent_of_code-02_25.cs
@@ -4,6 +4,8 @@
 // e.g 11-22 = 2 Invalid = 11, 22
 // 99-115 = 1 Invalid = 99
 
+// assumptions: no negative ID numbers
+
 List<string> input = 
 [
     "11-22","95-115","998-1012","1188511880-1188511890","222220-222224",
@@ -13,3 +15,33 @@ List<string> input =
 // List<string> input = [.. (await File.ReadAllTextAsync("day2_input.txt")).Split(",")];
 
 var result = 0; // Sum of All Invalid Numbers in each range
+
+foreach (var rangeInput in input)
+{
+    var numbers = rangeInput.Split("-");
+    int startNumber = int.Parse(numbers[0]);
+    int endNumber = int.Parse(numbers[1]);
+    var tensPlaces = ValidTensPlace(startNumber, endNumber);
+    if (tensPlaces.Count == 0) break;
+
+    for (int i = startNumber; i <= endNumber; i++)
+    {
+        
+    }
+}
+
+static List<int> ValidTensPlace(int startNumber, int endNumber)
+{
+    var exponent = 1;
+    
+    List<int> result = [];
+
+    var tensPlace = Math.Pow(10, exponent);
+
+    while (tensPlace >= startNumber || tensPlace <= endNumber)
+    {
+        result.Add((int) tensPlace);
+        exponent += 2;
+    }
+    return result;
+}
