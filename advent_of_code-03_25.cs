51374d590e908ca944ceff225b661a29a46006a1
Alexander <alexander.jee.t@gmail.com>
2025-12-08 23:07:56 -0600

    day 3 input with comments about problem

---
+++ advent_of_code-03_25.cs
@@ -0,0 +1,7 @@
+// List of digits separated by line ("bank")
+// Each digit represents value of jolt
+// Each tens place represents a "battery"
+// Get largest joltage for each bank
+// Can only select 2 digits ("batteries")
+// You get value of joltage by appending digits
+// e.g 811111111111119 = 89 = largest joltage
